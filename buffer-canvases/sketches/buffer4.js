a46816dd951d0e04c79969a21e19155f4fe430a7
looks <108483411+gamelooksfun@users.noreply.github.com>
2023-06-27 14:30:49 -0600

    Change res, weight, translate. Much better looking. Getting closer.

--- buffer-canvases/sketches/buffer4.js
+++ buffer-canvases/sketches/buffer4.js
@@ -1,8 +1,8 @@
 // My version
 
 // Setup global variables
-var noiseResolution = .03;
-var noiseGrid = 4;
+var noiseResolution = .005;
+var noiseGrid = 1;
 
 // Setup colors object
 const colors = {
@@ -21,12 +21,11 @@ col9 : [89, 89, 89],
 // Setup canvases
 function setup() {
     // Constant seed creates a consistent Perlin noise and random number sequence
-    seed = 1234;
+    seed = 9;
     noiseSeed(seed);
     randomSeed(seed);
     // Create initial canvas, noise buffer and object buffer
     createCanvas(400, 400);
-    // background(255);
     cnv1 = createGraphics(400, 400);
     cnv1.noStroke();
     cnv2 = createGraphics(400, 400);
@@ -36,7 +35,7 @@ function setup() {
     objectCanvas();
     finalDrawing();
     // Test image
-    // image(cnv2, 0, 0);
+    // image(cnv1, 0, 0);
 }
 
 // Noise buffer creates a grayscale noise field across the entire 400px cnv1
@@ -44,7 +43,7 @@ function noiseCanvas() {
     for (i = 0; i < width; i += noiseGrid) {
         for (j = 0; j < height; j += noiseGrid) {
             // Noise value
-            n = noise(i * noiseResolution, j * noiseResolution);
+            n = (noise(i * noiseResolution, j * noiseResolution)) * 3;
             // Draw a rectangle at this point with a fill value of n*255 and alpha of 100
             cnv1.fill(n * 255);
             cnv1.rect(i, j, noiseGrid);
@@ -68,10 +67,10 @@ function objectCanvas() {
 
 function finalDrawing() {
     background(255);
-    strokeWeight(7);
+    strokeWeight(1);
     colVary = 15;
-    for (i = 0; i < width; i += noiseGrid * 2.5) {
-      for (j = 0; j < height; j += noiseGrid * 2.5) {
+    for (i = 0; i < width; i += noiseGrid) {
+      for (j = 0; j < height; j += noiseGrid) {
             angCol = cnv1.get(i, j);
             ang = angCol[0] / 255 * PI * 2;
             col = cnv2.get(i, j);
@@ -81,9 +80,9 @@ function finalDrawing() {
                 col[2]+random(-colVary,colVary),
                 255);
             push();
-            translate(i, j);
+            translate(0, 0);
             rotate(ang);
-            line(0,0,noiseGrid,0);
+            ellipse(i,j,.1);
             pop();
         }
     }
